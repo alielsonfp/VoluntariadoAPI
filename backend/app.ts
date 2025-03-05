f3b3117bc5ccfa733b12e56634626548788fb2e8
alielsonfp <alielson_pequeno@hotmail.com>
2025-03-05 19:58:59 -0300

    feat: Adicionado activityRoutes ao app.ts
    - Adicionado as rotas de atividades (ctivityRoutes) ao pp.ts.
    - Todas as rotas de atividades agora estão disponíveis em /api/activities.

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
-
+import activityRoutes from './routes/activityRoutes'; // Importar as rotas de atividades
 
 dotenv.config();
 
@@ -10,8 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Rotas de autenticação
 app.use('/api/auth', authRoutes);
 
+// Rotas de atividades
+app.use('/api/activities', activityRoutes); // Adicionar as rotas de atividades
+
+// Rota de teste
 app.get('/', (req, res) => {
   res.send('API de Atividades de Voluntariado');
 });
